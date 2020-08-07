37dbf58a962ad4fd9c69dd584bc17fbdd0080e2a
Advay-Inabathini <iadvay@gmail.com>
2020-08-07 15:17:10 +0530

    replaced scroll-margin and smooth scrolling with javascript

--- assets/js/booksyndy.js
+++ assets/js/booksyndy.js
@@ -6,8 +6,36 @@ $(document).ready(function(){
 	$('#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
-	$('#navbarNav a').on('click', function(){
+	$('.navbar a').on('click', function(){
         $("#navbarNav").removeClass("show")
 		$("#nav-icon4").toggleClass("open")
+		if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		var hash = this.hash;
+		$('html, body').animate({ scrollTop: $(hash).offset().top - 55 },750);
+		}
     });
 });
+
+
+// function add_smooth_scroll_to_hash_anim_on_sidebar_click() {
+//     $(".sidebar-menu a").on('click', function (event) {
+
+//         // Make sure this.hash has a value before overriding default behavior
+//         if (this.hash !== "") {
+
+//             // Prevent default anchor click behavior
+//             event.preventDefault();
+
+//             // Store hash
+//             var hash = this.hash;
+
+//             // Using jQuery's animate() method to add smooth page scroll
+//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//             $('html, body').animate({ scrollTop: $(hash).offset().top - 100 }, 800);
+//         } // End if
+//     });
+// }

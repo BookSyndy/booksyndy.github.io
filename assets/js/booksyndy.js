aa9ab7f9f1a6d16cb7bc06f44e222a076865581a
Advay-Inabathini <iadvay@gmail.com>
2020-08-07 07:57:10 +0530

    navbar collapses on clicking navigation link

--- assets/js/booksyndy.js
+++ assets/js/booksyndy.js
@@ -6,4 +6,8 @@ $(document).ready(function(){
 	$('#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
+	$('#navbarNav a').on('click', function(){
+        $("#navbarNav").removeClass("show")
+		$("#nav-icon4").toggleClass("open")
+    });
 });

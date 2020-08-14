39945d8cc0855c6590d0e64b7e6f1f58cbf16e43
Advay-Inabathini <iadvay@gmail.com>
2020-08-14 20:13:26 +0530

    basic carousel for testimonials setup

--- assets/js/booksyndy.js
+++ assets/js/booksyndy.js
@@ -61,6 +61,22 @@ $('.step').on('click', function(){
     $(this).addClass('selected');
 });
 
+// $(".carousel").on("touchstart", function(event){
+// 	var xClick = event.originalEvent.touches[0].pageX;
+// $(this).one("touchmove", function(event){
+// 	var xMove = event.originalEvent.touches[0].pageX;
+// 	if( Math.floor(xClick - xMove) > 5 ){
+// 		$(this).carousel('next');
+// 	}
+// 	else if( Math.floor(xClick - xMove) < -5 ){
+// 		$(this).carousel('prev');
+// 	}
+// });
+// $(".carousel").on("touchend", function(){
+// 		$(this).off("touchmove");
+// });
+// });
+
 // var myimg= document.getElementById("imgDisp");
 // var images=[
 //     "./assets/img/buy_sell.png",

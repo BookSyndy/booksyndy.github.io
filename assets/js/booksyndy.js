9e92ac13e3d2e5533f8a3b1cbf4e3400d7d73093
Advay-Inabathini <iadvay@gmail.com>
2020-08-22 16:58:13 +0530

    javascript file cleaned up

--- assets/js/booksyndy.js
+++ assets/js/booksyndy.js
@@ -66,42 +66,6 @@ $('.carousel').carousel({
 	ride: "carousel",
   })
 
-// $(".carousel").on("touchstart", function(event){
-// 	var xClick = event.originalEvent.touches[0].pageX;
-// $(this).one("touchmove", function(event){
-// 	var xMove = event.originalEvent.touches[0].pageX;
-// 	if( Math.floor(xClick - xMove) > 5 ){
-// 		$(this).carousel('next');
-// 	}
-// 	else if( Math.floor(xClick - xMove) < -5 ){
-// 		$(this).carousel('prev');
-// 	}
-// });
-// $(".carousel").on("touchend", function(){
-// 		$(this).off("touchmove");
-// });
-// });
-
-// var myimg= document.getElementById("imgDisp");
-// var images=[
-//     "./assets/img/buy_sell.png",
-// 	"./assets/img/profile.png",
-// 	"./assets/img/download.png",
-// ];
-
-// function change(){
-// 	myimg.src= images.reverse()[0];
-// };
-
-// setInterval(change,1000); 
-
-// function gatekeeper(esno){
-// 	var yesno = esno;
-// }
-
-// $('.step').click(function(){
-// 	$(this).toggleClass('clicked');
-// });
 console.log("Hello there, fellow Inspector")
 console.log("Welcome to BookSyndy's website")
 console.log("Designed and Developed by Advay Sai Inabathini")
